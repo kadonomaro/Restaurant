eb4577828755a261d5abf1c521631b97e2d9b0b8
kadonomaro <denis.afer@gmail.com>
2019-01-27 21:31:19 +0700

    Accordion for menu items

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function () {
+
+    let menuBtn = document.querySelectorAll('.menu-card__title');
+    let menuContent = document.querySelectorAll('.menu-card__content');
+    for (let i = 0; i < menuBtn.length; i++) {
+        menuBtn[i].addEventListener('click', function () {
+            console.dir(menuContent[i]);
+            
+            if (menuContent[i].clientHeight === menuContent[i].scrollHeight) {
+                menuContent[i].style.maxHeight = 0 + 'px';
+            }
+            else {
+                menuContent[i].style.maxHeight = menuContent[i].scrollHeight + 'px';
+            }
+
+
+        })
+        
+    }
+
+});
