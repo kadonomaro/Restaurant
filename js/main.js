144153c98c556be6ea1ad230af96bd5821203c2b
kadonomaro <denis.afer@gmail.com>
2019-01-29 13:07:07 +0700

    added scroll-top button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function () {
 
+    //аккордион для секции "Меню"
     let menuBtn = document.querySelectorAll('.menu-card__title');
     let menuContent = document.querySelectorAll('.menu-card__content');
     for (let i = 0; i < menuBtn.length; i++) {
@@ -13,10 +14,18 @@ window.addEventListener('load', function () {
             else {
                 menuContent[i].style.maxHeight = menuContent[i].scrollHeight + 'px';
             }
-
-
         })
-        
     }
+
+    //Добавление появления кнопки для скролла в начало страницы
+    let scrollTopButton = document.querySelector('.scroll-top');
+    window.addEventListener('scroll', function () {
+
+        if (window.pageYOffset > window.innerHeight) {
+            scrollTopButton.classList.add('scroll-top_active');
+        } else {
+            scrollTopButton.classList.remove('scroll-top_active');
+        }
+    });
 
 });
